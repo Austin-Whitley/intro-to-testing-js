70ba9507ccb42aefa501f655b549262b57824507
Austin Whitley <whitley.austin01@gmail.com>
2021-03-24 19:07:34 -0500

    finished isEven test suite

--- code.js
+++ code.js
@@ -12,3 +12,9 @@ function sayHello(input){
         return "Hello, World!";
     }
 }
+function isFive(input){
+    return parseFloat(input) === 5;
+}
+function isEven(input){
+    return parseFloat(input) % 2 === 0;
+}
--- tests.js
+++ tests.js
@@ -58,6 +58,60 @@ describe("sayHello", function(){
     it('should return "Hello, World! when a NULL value is passed.', function () {
         expect(sayHello()).toBe("Hello, World!");
     });
-
-
 });
+describe("isFive", function(){
+    it('should be a defined function', function () {
+        expect(typeof isFive).toBe("function");
+    });
+    it('should return a boolean value', function () {
+        expect(typeof isFive()).toBe('boolean')
+    });
+    it('should return the boolean value false when passed the number 4', function () {
+        expect(isFive(4)).toBe(false);
+    });
+    it('should return false when passed the string "yes"', function () {
+        expect(isFive("yes")).toBe(false);
+    });
+    it('should not be undefined', function () {
+        expect(isFive()).not.toBe(undefined);
+    });
+    it('should return true when paseed the number 5', function () {
+        expect(isFive(5)).toBe(true);
+    });
+    it('should return true when passed "5"', function () {
+        expect(isFive("5")).toBe(true);
+    });
+});
+describe("isEven", function(){
+    it('should return a boolean value no matter the input', function () {
+        expect(typeof isEven()).toBe('boolean');
+    });
+    it('should return true when passed the number 2', function () {
+        expect(isEven(2)).toBe(true);
+    });
+    it('should return true when passed the number -4', function () {
+        expect(isEven(-4)).toBe(true);
+    });
+    it('should return false when passed the number 3', function () {
+        expect(isEven(3)).toBe(false);
+    });
+    it('should return false when passed the string "bananas"', function () {
+        expect(isEven("bananas")).toBe(false);
+    });
+    it('should return true when passed "8"', function () {
+        expect(isEven("8")).toBe(true);
+    });
+    it('should return false when passed Infinity', function () {
+        expect(isEven(Infinity)).toBe(false);
+    });
+    it('should return false when passed a boolean value false', function () {
+        expect(isEven(false)).toBe(false);
+    });
+    it('should return false when passed an empty argument', function () {
+        expect(isEven()).toBe(false);
+    });
+});
+
+
+
+
